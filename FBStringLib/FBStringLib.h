4362f1f303d47dbd3a3fca0a4ab10a871e53c0a5
Jungwan Byun <jungwan82@naver.com>
2015-11-08 21:08:41 +0900

    compile buf fixes on Mac

--- FBStringLib/FBStringLib.h
+++ FBStringLib/FBStringLib.h
@@ -36,10 +36,9 @@ namespace fastbird{
 
 	//-----------------------------------------------------------------------
 	// UNICODE conversion
-	enum EUnicodeByteOrder
-	{
-		LITTLE_ENDIAN,
-		BIG_ENDIAN,
+	enum EUnicodeByteOrder	{
+		FB_LITTLE_ENDIAN,
+		FB_BIG_ENDIAN,
 	};
 
 	/// This function will attempt to decode a UTF-8 encoded character in the buffer.
@@ -52,11 +51,11 @@ namespace fastbird{
 
 	/// This function will attempt to decode a UTF-16 encoded character in the buffer.
 	/// If the encoding is invalid, the function returns -1.
-	int DecodeUTF16(const unsigned char *encodedBuffer, unsigned int *outCharLength, EUnicodeByteOrder byteOrder = LITTLE_ENDIAN);
+	int DecodeUTF16(const unsigned char *encodedBuffer, unsigned int *outCharLength, EUnicodeByteOrder byteOrder = FB_LITTLE_ENDIAN);
 
 	/// This function will encode the value into the buffer.
 	/// If the value is invalid, the function returns -1, else the encoded length.
-	int EncodeUTF16(unsigned int value, unsigned char *outEncodedBuffer, unsigned int *outCharLength, EUnicodeByteOrder byteOrder = LITTLE_ENDIAN);
+	int EncodeUTF16(unsigned int value, unsigned char *outEncodedBuffer, unsigned int *outCharLength, EUnicodeByteOrder byteOrder = FB_LITTLE_ENDIAN);
 
 	/// return data is temporary data. save it to other memory if you need
 	unsigned char* AnsiToUTF8(const char* source);
